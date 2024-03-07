62e7e390b639307d87255162652e60e555890a70
kiu_maghlakelidze.saba <maghlakelidze.saba@kiu.edu.ge>
2024-03-07 21:21:19 +0400

    modified next.config to receive remotely and images are visible now

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.pexels.com',
+                port: '',
+                // ** is a wildcard, which means anything goes
+                pathname: '/photos/**'
+            }
+        ]
+    }
+};
 
 export default nextConfig;
