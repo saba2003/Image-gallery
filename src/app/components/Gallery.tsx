ccf41b4eb1489e4e15960518e54447b45c80ef78
kiu_maghlakelidze.saba <maghlakelidze.saba@kiu.edu.ge>
2024-05-10 20:30:10 +0400

    gave image containers proper height/width

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -24,7 +24,8 @@ export default async function Gallery({ topic }: Props) {
     const photosWithBlur = await addBlurredDataUrls(images)
 
     return (
-        <section className="px-2 my-3 grid gap-2 grid-cols-gallery">
+        <section className="px-1 my-3 grid
+        grid-cols-gallery auto-rows-[10px]">
             
             {photosWithBlur.map(photo => (
                 <ImgContainer key={photo.id} photo={photo} />
